eb58edb87202c0a6bafaf41d1ba70274e6194903
Víctor Gracia Magallón <info@victorgraciaweb.com>
2024-09-14 13:34:41 +0300

    Excpetions adapter

--- src/common/adapters/axios.adapter.ts
+++ src/common/adapters/axios.adapter.ts
@@ -2,19 +2,23 @@ import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 import { Injectable } from "@nestjs/common";
 
 import { HttpAdapter } from "../interfaces/http-adapter.interface";
+import { ExceptionHandlerService } from "../services/exception-handler.service";
 
 @Injectable()
 export class AxiosAdapter implements HttpAdapter {
 
     private axios: AxiosInstance = axios;
 
+    constructor(
+        private readonly exceptionHandlerService: ExceptionHandlerService
+    ) {}
+
     async get<T>(url: string, config?: AxiosRequestConfig): Promise<T> {
         try {
             const { data } = await this.axios.get<T>(url, config);
             return data;
         } catch (error) {
-            // Maneja el error adecuadamente
-            throw new Error(`Error fetching data from ${url}: ${error.message}`);
+            this.exceptionHandlerService.handleExceptions(error);
         }
     }
 }
