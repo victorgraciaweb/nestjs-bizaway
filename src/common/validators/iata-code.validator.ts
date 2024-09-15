69cf004ba8a2f245e43f08168e812c682522f8dd
Víctor Gracia Magallón <info@victorgraciaweb.com>
2024-09-15 21:47:04 +0300

    Remove comments

--- src/common/validators/iata-code.validator.ts
+++ src/common/validators/iata-code.validator.ts
@@ -4,13 +4,11 @@ import iataCodes from '../data/iata-codes.json';
 @ValidatorConstraint({ name: 'isIataCode', async: false })
 export class IsIataCode implements ValidatorConstraintInterface {
     validate(code: string, args: ValidationArguments): boolean {
-        // Check if the code is in the list of valid IATA codes
         return iataCodes.includes(code);
     }
 
     defaultMessage(args: ValidationArguments): string {
-        // Determine the property name that is invalid
-        const property = args.property; // This is either 'origin' or 'destination'
+        const property = args.property;
         return `The ${property} code $value is not a valid IATA code.`;
     }
 }
