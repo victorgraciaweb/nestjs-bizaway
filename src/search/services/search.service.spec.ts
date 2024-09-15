0aa33be549d6eba98dc7a845847a2dd1ebb0b8dc
Víctor Gracia Magallón <info@victorgraciaweb.com>
2024-09-15 19:01:44 +0300

    Changes in test search

--- src/search/services/search.service.spec.ts
+++ src/search/services/search.service.spec.ts
@@ -67,13 +67,13 @@ describe('SearchService', () => {
         };
 
         const mockResponse = [
-            { origin: 'NYC', destination: 'LAX', duration: 300, cost: 100 },
-            { origin: 'NYC', destination: 'LAX', duration: 400, cost: 80 },
+            { origin: 'FRA', destination: 'BCN', cost: 559, duration: 5, type: 'car', id: '18c80f09-2e29-499a-aba1-98c20a856244', display_name: 'from FRA to BCN by car' },
+            { origin: 'FRA', destination: 'BCN', cost: 983, duration: 6, type: 'train', id: 'd96f711a-8aae-44fe-9c04-5d10ed36c052', display_name: 'from FRA to BCN by train' },
         ];
 
         const mappedResponse = [
-            { id: '18c80f09-2e29-499a-aba1-98c20a856244', display_name: 'Trip 1', origin: 'NYC', destination: 'LAX', duration: 300, type: 'car', cost: 100 },
-            { id: 'd96f711a-8aae-44fe-9c04-5d10ed36c052', display_name: 'Trip 2', origin: 'NYC', destination: 'LAX', duration: 400, type: 'flight', cost: 80 },
+            { id: '18c80f09-2e29-499a-aba1-98c20a856244', origin: 'NYC', destination: 'LAX', cost: 100, duration: 300, type: 'car', display_name: 'Trip 1' },
+            { id: 'd96f711a-8aae-44fe-9c04-5d10ed36c052', origin: 'NYC', destination: 'LAX', cost: 450, duration: 200, type: 'car', display_name: 'Trip 2' },
         ];
 
         jest.spyOn(httpMock, 'get').mockResolvedValue(mockResponse);
@@ -90,6 +90,41 @@ describe('SearchService', () => {
         expect(mappingServiceMock.mapToResponseSearchTripDto).toHaveBeenCalledWith(mockResponse);
         expect(result).toEqual(mappedResponse);
     });
+
+
+    it('should call http.get and return mapped trips', async () => {
+        // Arrange
+        const searchTripDto: SearchTripDto = {
+            origin: 'NYC',
+            destination: 'LAX',
+            sort_by: 'cheapest',
+        };
+
+        const mockResponse = [
+            { origin: 'FRA', destination: 'BCN', cost: 559, duration: 5, type: 'car', id: '18c80f09-2e29-499a-aba1-98c20a856244', display_name: 'from FRA to BCN by car' },
+            { origin: 'FRA', destination: 'BCN', cost: 983, duration: 6, type: 'train', id: 'd96f711a-8aae-44fe-9c04-5d10ed36c052', display_name: 'from FRA to BCN by train' },
+        ];
+
+        const mappedResponse = [
+            { id: '18c80f09-2e29-499a-aba1-98c20a856244', origin: 'NYC', destination: 'LAX', cost: 100, duration: 300, type: 'car', display_name: 'Trip 1' },
+            { id: 'd96f711a-8aae-44fe-9c04-5d10ed36c052', origin: 'NYC', destination: 'LAX', cost: 450, duration: 200, type: 'car', display_name: 'Trip 2' },
+        ];
+
+        jest.spyOn(httpMock, 'get').mockResolvedValue(mockResponse);
+        jest.spyOn(mappingServiceMock, 'mapToResponseSearchTripDto').mockReturnValue(mappedResponse);
+
+        // Act
+        const result = await service.findTrips(searchTripDto);
+
+        // Assert
+        expect(httpMock.get).toHaveBeenCalledWith('http://api.bizaway.com/trips', {
+            headers: { 'x-api-key': 'dummy-api-key' },
+            params: { origin: 'NYC', destination: 'LAX' },
+        });
+        expect(mappingServiceMock.mapToResponseSearchTripDto).toHaveBeenCalledWith(mockResponse);
+        expect(result).toEqual(mappedResponse);
+    });
+
 
     it('should call ExceptionHandlerService on error', async () => {
         // Arrange
