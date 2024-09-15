3398317e787573e8dc2951dc521e7ae85486f0be
Víctor Gracia Magallón <info@victorgraciaweb.com>
2024-09-15 18:26:17 +0300

    Service test search

---
+++ src/search/services/search.service.spec.ts
@@ -0,0 +1,110 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { SearchService } from './search.service';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
+import { ConfigService } from '@nestjs/config';
+import { ExceptionHandlerService } from 'src/common/services/exception-handler.service';
+import { MappingService } from 'src/common/services/mapping.service';
+import { SearchTripDto } from '../dto';
+
+describe('SearchService', () => {
+    let service: SearchService;
+    let httpMock: AxiosAdapter;
+    let mappingServiceMock: MappingService;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                SearchService,
+                {
+                    provide: AxiosAdapter,
+                    useValue: {
+                        get: jest.fn(), // Mock de AxiosAdapter
+                    },
+                },
+                {
+                    provide: ConfigService,
+                    useValue: {
+                        get: jest.fn((key: string) => {
+                            switch (key) {
+                                case 'urlBizaway':
+                                    return 'http://api.bizaway.com/trips';
+                                case 'apiKeyBizaway':
+                                    return 'dummy-api-key';
+                                default:
+                                    return null;
+                            }
+                        }),
+                    },
+                },
+                {
+                    provide: ExceptionHandlerService,
+                    useValue: {
+                        handleExceptions: jest.fn(),
+                    },
+                },
+                {
+                    provide: MappingService,
+                    useValue: {
+                        mapToResponseSearchTripDto: jest.fn(),
+                    },
+                },
+            ],
+        }).compile();
+
+        service = module.get<SearchService>(SearchService);
+        httpMock = module.get<AxiosAdapter>(AxiosAdapter);
+        mappingServiceMock = module.get<MappingService>(MappingService);
+    });
+
+    it('should call http.get and return mapped trips', async () => {
+        // Arrange
+        const searchTripDto: SearchTripDto = {
+            origin: 'NYC',
+            destination: 'LAX',
+            sort_by: 'fastest',
+        };
+
+        const mockResponse = [
+            { origin: 'NYC', destination: 'LAX', duration: 300, cost: 100 },
+            { origin: 'NYC', destination: 'LAX', duration: 400, cost: 80 },
+        ];
+
+        const mappedResponse = [
+            { id: '18c80f09-2e29-499a-aba1-98c20a856244', display_name: 'Trip 1', origin: 'NYC', destination: 'LAX', duration: 300, type: 'car', cost: 100 },
+            { id: 'd96f711a-8aae-44fe-9c04-5d10ed36c052', display_name: 'Trip 2', origin: 'NYC', destination: 'LAX', duration: 400, type: 'flight', cost: 80 },
+        ];
+
+        jest.spyOn(httpMock, 'get').mockResolvedValue(mockResponse);
+        jest.spyOn(mappingServiceMock, 'mapToResponseSearchTripDto').mockReturnValue(mappedResponse);
+
+        // Act
+        const result = await service.findTrips(searchTripDto);
+
+        // Assert
+        expect(httpMock.get).toHaveBeenCalledWith('http://api.bizaway.com/trips', {
+            headers: { 'x-api-key': 'dummy-api-key' },
+            params: { origin: 'NYC', destination: 'LAX' },
+        });
+        expect(mappingServiceMock.mapToResponseSearchTripDto).toHaveBeenCalledWith(mockResponse);
+        expect(result).toEqual(mappedResponse);
+    });
+
+    it('should call ExceptionHandlerService on error', async () => {
+        // Arrange
+        const searchTripDto: SearchTripDto = {
+            origin: 'NYC',
+            destination: 'LAX',
+            sort_by: 'fastest',
+        };
+
+        const apiError = new Error('API Error');
+        jest.spyOn(httpMock, 'get').mockRejectedValue(apiError);
+      
+        // Spy directamente en el mock configurado en el beforeEach
+        const handleExceptionsSpy = jest.spyOn(service['exceptionHandler'], 'handleExceptions');
+      
+        // Act & Assert
+        await expect(service.findTrips(searchTripDto)).rejects.toThrow('API Error');
+        expect(handleExceptionsSpy).toHaveBeenCalledWith(apiError);
+    });
+});
